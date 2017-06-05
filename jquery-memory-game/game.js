f8ca0301f26a91927b6c9c3ec1774ebefb1d7667
Aksel Nooitgedagt <akselon@gmail.com>
2017-06-05 19:59:44 +0200

    Source files for a jQuery tutorial

---
+++ jquery-memory-game/game.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+  // Definicja zmiennych gry
+  var wybranyKlocek = 0;
+  var liczbaZnalezionych = 0;
+  var liczbaProb = 0;
+
+  function aktualizujStatus () {
+    $('#status-gry').html('Znaleziono: '+liczbaZnalezionych+', Próby: '+liczbaProb);
+  }
+
+  // Przy kliknięciu w element o klasie przeslona, uruchamiamy poniższy kod
+  $('.przeslona').click(function() {
+    // Ktoś nakliknął przesłonę, więc ją ukrywamy (tutaj: this odnosi się do klikniętego elementu)
+    $(this).hide();
+
+    // Sprawdzamy jaki numer klocka został kliknięty. Pobieramy ojca przesłony, czyli element nad przesłoną
+    var kliknietyKlocek = $(this).parent().attr('mem-number');
+
+    if (wybranyKlocek == 0) {
+        // Jeżeli żaden klocek nie był jeszcze wybrany, to przypisujemy numer wybranego klocka
+      wybranyKlocek = kliknietyKlocek;
+    } else if (wybranyKlocek == kliknietyKlocek) {
+      // Jeżeli wybrany wcześniej klocek ma ten sam numer, co kliknięty klocek, to znaleźliśmy parę
+      liczbaZnalezionych++;
+      liczbaProb++;
+
+      $('#informacja-znaleziono').slideDown();
+      setTimeout(function() {
+        // Ukrywany każdy klocek, którego atrybut mem-number jest równy numerowi wybranego klocka
+        // $('[mem-number='+kliknietyKlocek+'] img').fadeOut();
+        $('[mem-number='+kliknietyKlocek+'] .przeslona').remove();
+        $('#informacja-znaleziono').slideUp();
+        if (liczbaZnalezionych == 7) {
+          alert('Udało Ci się znaleźć wszystkie pary, gratulacje!');
+        }
+      }, 2000);
+      wybranyKlocek = 0;
+    } else {
+      liczbaProb++;
+      var doZasloniecia1 = wybranyKlocek;
+      var doZasloniecia2 = kliknietyKlocek;
+      wybranyKlocek = 0;
+      setTimeout(function() {
+        $('[mem-number='+doZasloniecia1+'] .przeslona').fadeIn('slow');
+        $('[mem-number='+doZasloniecia2+'] .przeslona').fadeIn('slow');
+      }, 1000);
+    }
+    // Po każdym kliknięciu aktualizujemy status gry
+    aktualizujStatus();
+  });
+});
